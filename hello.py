ba0126f6983554f08f274281fd65dc28fd2560c6
whiterabbitsource <meghandoerr@gmail.com>
2020-08-11 22:00:31 -0400

    Strings minor update

--- hello.py
+++ hello.py
@@ -2,5 +2,12 @@
 print("Hello, World!")
 # Learning Strings
 my_string = "This is a string"
+## Make string uppercase
 my_string_upper = my_string.upper()
 print(my_string_upper)
+# Determine data type of string
+print(type(my_string))
+# Slicing strings [python is zero-based and starts at 0 and not 1]
+print(my_string[0:4])
+print(my_string[:1])
+print(my_string[0:14])
